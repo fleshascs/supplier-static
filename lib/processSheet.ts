8e0f4d8b5548b3123f0ed18b23cf6d3f6fcdd231
fleshascs <35168446+fleshascs@users.noreply.github.com>
2023-01-14 22:21:33 +0200

    cleanup

--- lib/processSheet.ts
+++ lib/processSheet.ts
@@ -35,9 +35,8 @@ export async function processSheet(workbook, searchQueries: string[], categories
 }
 
 export function getCategories(rows: Row[], categoriesColumn: string) {
-  const categories = new Set();
-  rows.forEach((item) => {
-    item[categoriesColumn].split(',').forEach((c) => categories.add(c));
-  });
-  return categories;
+  return rows.reduce((set, row) => {
+    row[categoriesColumn].split(',').forEach((c) => set.add(c));
+    return set;
+  }, new Set());
 }
